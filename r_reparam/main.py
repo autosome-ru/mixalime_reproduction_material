6a406c2ceda776c5a9be835d042a985b7a68c050
georgy.m <iam@georgy.top>
2023-06-16 12:25:15 +0300

    Remove xlim for slices

--- r_reparam/main.py
+++ r_reparam/main.py
@@ -22,3 +22,20 @@ plt.ylabel(r'$\kappa$')
 plt.tight_layout()
 
 plt.savefig('reparam.pdf')
+
+
+# kappas = [2, 5, 10, 50]
+rs = [5, 10, 20, 40]
+kappas = np.linspace(2, 200, n)
+plt.figure(figsize=(6, 6), dpi=200)
+legend = list()
+its = []
+for r in rs:
+    rt = calc_r(r, kappas)
+    its.append(plt.plot(kappas, rt)[0]);plt.axhline(r, linestyle='--', color=its[-1].get_color(), linewidth=1)
+    legend.append(r'$r = ' + str(r) + r'$')
+plt.legend(labels=legend, handles=its)
+plt.xlabel('$\kappa$')
+plt.ylabel(r'$r_{BetaNB}\left(r, \kappa, \frac{1}{2}\right)$')
+plt.tight_layout()
+plt.savefig('reparam_1d.pdf')
